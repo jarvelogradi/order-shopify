db9fc1aabf7d9d56c7ce76d6d9958c2b88c69131
Moises Arvelo <moisesarvelo45@gmail.com>
2022-05-18 18:19:28 -0400

    Cambio endpoint orders count

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const URL_BASE = `https://${config.API_KEY}:${config.API_TOKEN}@${config.SHOP_UR
 // Request's order
 
 app.get("/", async(req, res) =>{
-    const response = await fetch(`${URL_BASE}admin/api/2021-10/orders.json?status=any`, {
+    const response = await fetch(`${URL_BASE}admin/api/2021-10/orders/count.json?status=any`, {
         method: 'GET'
     })
     let data = await response.json()  
