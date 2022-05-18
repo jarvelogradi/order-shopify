b864b0dc2f57a107cc99bc86afe54eb48bcedc57
Moises Arvelo <moisesarvelo45@gmail.com>
2022-05-18 18:06:18 -0400

    Change

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ const URL_BASE = `https://${config.API_KEY}:${config.API_TOKEN}@${config.SHOP_UR
 // Request's order
 
 app.get("/", async(req, res) =>{
-    const response = await fetch(`${URL_BASE}admin/api/2021-10/orders.json?status=any`, {
+    /*const response = await fetch(`${URL_BASE}admin/api/2021-10/orders.json?status=any`, {
         method: 'GET'
     })
-    let data = await response.json()  
+    let data = await response.json()*/
 
-    res.send(data);
+    res.send("Hola Mundo");
 })
 
 app.listen(port, ()=>{
